45deb132c920b23ded7f65939d24812655335753
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-11-14 13:01:11 -0300

    mudando css e leitura do json

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-import data from './data.json' assert { type: 'json' };
+fetch('./data.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const { partidas, oitavas, quartas, semifinal, final } = data;
 
-const { partidas, oitavas, quartas, semifinal, final } = data;
+    // console.log(partidas, oitavas, quartas, semifinais, final);
 
-// console.log(partidas, oitavas, quartas, semifinais, final);
+    function convertCharacters(string) {
+      const text = string.replaceAll(' ', '_').toLowerCase();
+      return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    }
 
-function convertCharacters(string) {
-  const text = string.replaceAll(' ', '_').toLowerCase();
-  return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-}
-
-function createGame(games) {
-  console.log(games);
-  let list_games = '';
-  for (const {
-    player1,
-    goals_player1,
-    hour,
-    goals_player2,
-    player2,
-  } of games) {
-    list_games += `
+    function createGame(games) {
+      console.log(games);
+      let list_games = '';
+      for (const {
+        player1,
+        goals_player1,
+        hour,
+        goals_player2,
+        player2,
+      } of games) {
+        list_games += `
     <li>
       <img src="./assets/icons/${convertCharacters(
         player1
@@ -32,17 +33,17 @@ function createGame(games) {
       )}.svg" alt="Bandeira da ${player2}" />
     </li>
   `;
-  }
+      }
 
-  return list_games;
-}
+      return list_games;
+    }
 
-function createCard(array_games) {
-  let delay = -0.4;
-  let list_games = '';
-  for (const { date, day_week, games } of array_games) {
-    delay += 0.4;
-    list_games += `
+    function createCard(array_games) {
+      let delay = -0.4;
+      let list_games = '';
+      for (const { date, day_week, games } of array_games) {
+        delay += 0.4;
+        list_games += `
       <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day_week}</span></h2>
         <ul>
@@ -50,15 +51,16 @@ function createCard(array_games) {
         </ul>
       </div>
     `;
-  }
+      }
 
-  return list_games;
-}
+      return list_games;
+    }
 
-document.querySelector('#partidas').innerHTML = createCard(partidas);
-document.querySelector('#oitavas').innerHTML = createCard(oitavas);
-document.querySelector('#quartas').innerHTML = createCard(quartas);
-document.querySelector('#semifinal').innerHTML = createCard(semifinal);
-document.querySelector('#final').innerHTML = createCard(final);
+    document.querySelector('#partidas').innerHTML = createCard(partidas);
+    document.querySelector('#oitavas').innerHTML = createCard(oitavas);
+    document.querySelector('#quartas').innerHTML = createCard(quartas);
+    document.querySelector('#semifinal').innerHTML = createCard(semifinal);
+    document.querySelector('#final').innerHTML = createCard(final);
 
-// createGame('Catar', '16:00')
+    // createGame('Catar', '16:00')
+  });
